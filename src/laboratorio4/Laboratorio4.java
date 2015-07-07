ae5a231c96f940940b70ff2e3785496dd654f22c
panlopezv <panlopezv@outlook.com>
2015-07-07 10:55:27 -0600

    Creacion de proyecto

---
+++ src/laboratorio4/Laboratorio4.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laboratorio4;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author EST1629311
+ */
+public class Laboratorio4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ArrayList<Integer> arreglo = new ArrayList<>();
+    }
+    
+}
